af02c3c155905121b96ac832b15d69762d5c41d8
Bryant Kyle Sagun <bryantkyle.sagun@gmail.com>
2020-05-05 23:05:20 +0800

    sticky header completed

--- app/scripts/App.js
+++ app/scripts/App.js
@@ -1,8 +1,10 @@
 import '../assets/styles/styles.css';
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll.js';
+import StickyHeader from './modules/StickyHeader.js';
 
 let mobileMenu = new MobileMenu();
+let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
 
