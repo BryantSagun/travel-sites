17a13eb0c1614824356f3483f3e99f7575d1251c
Bryant Kyle Sagun <bryantkyle.sagun@gmail.com>
2020-05-06 02:34:17 +0800

    modal completed

--- app/scripts/App.js
+++ app/scripts/App.js
@@ -3,10 +3,25 @@ import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll.js';
 import StickyHeader from './modules/StickyHeader.js';
 
-let mobileMenu = new MobileMenu();
-let stickyHeader = new StickyHeader();
+let modal;
+new MobileMenu();
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+
+document.querySelectorAll(".open-modal").forEach(el => {
+     el.addEventListener("click", e=> {
+          e.preventDefault();
+          if(typeof modal == "undefined"){
+               import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+                    modal = new x.default();
+                    setTimeout(() => modal.openTheModal(), 20)
+               }).catch(() => console.log("Modal cannot be loaded."));
+          } else{
+               modal.openTheModal();
+          }
+     })
+})
 
 if(module.hot){
      module.hot.accept();
