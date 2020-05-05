c5d806b874d4e7ffd707cd62f5b92cf4c8636cfe
Bryant Kyle Sagun <bryantkyle.sagun@gmail.com>
2020-05-06 04:37:27 +0800

    lazy loading completed

--- app/scripts/App.js
+++ app/scripts/App.js
@@ -1,4 +1,5 @@
 import '../assets/styles/styles.css';
+import 'lazysizes';
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll.js';
 import StickyHeader from './modules/StickyHeader.js';
